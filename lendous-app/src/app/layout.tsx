3200609ca573d32344e16ae01abcaee1d50a54eb
Divine Ebube Ifechukwude <difebi14@gmail.com>
2025-06-01 15:08:16 +0200

    Update layout.tsx

--- lendous-app/src/app/layout.tsx
+++ lendous-app/src/app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className="">
+      <body>
         <Navbar />
-        <div className="">
+        <div>
           {children}
         </div>
         <Footer />
